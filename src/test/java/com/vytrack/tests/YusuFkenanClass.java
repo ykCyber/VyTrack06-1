52166f934b033b7cb3ae9071f9191f9f753c691a
ykCyber <yusufkenan2604@gmail.com>
2020-05-23 17:12:51 +0300

    sout

--- src/test/java/com/vytrack/tests/YusuFkenanClass.java
+++ src/test/java/com/vytrack/tests/YusuFkenanClass.java
@@ -4,5 +4,6 @@ public class YusuFkenanClass {
     public static void main(String[] args) {
         System.out.println("hadi hayırlısı");
         System.out.println("ben bu işi anlamadım");
+        System.out.println("nasıl olacak bu iş?");
     }
 }
